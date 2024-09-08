681865b47abbbf754dc634a32f713e4830dbb02b
carlosmding <carlos.pinto@udea.edu.co>
2024-09-08 18:39:04 -0500

    Se incluye la validación de parámetros de entrada al programa según las indicaciones de la práctica y además se describen las funciones que deben estar en cada escenario

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
  //Creación de main inicial
  printf("Práctica #1 - Sistemas Operativos\n");
- return 0;
+
+ //Validación de los diferentes escenarios
+ if (argc == 1) { 
+    printf("Primer escenario (Sin archivo de entrada ni salida)\n");
+    //Función para leer desde pantalla Stdin
+    //Funcion para invertir el texto
+    //Imprimir el resultado final en pantalla
+    exit(0); }
+
+ else if (argc == 2) { 
+    printf("Segundo escenario (Sólo archivo de entrada)\n");
+    //Función para leer el archivo de entrada
+    //Funcion para invertir el texto
+    //Imprimir el resultado final en pantalla 
+    exit(0); }
+
+ else if (argc == 3) { 
+    printf("Tercer escenario (Archivo de entrada y salida)\n");
+    //Función para leer el archivo de entrada
+    //Funcion para invertir el texto
+    //Función para escribir el texo invertido en archivo de salida
+    exit(0); }
+
+ else if (argc > 3) { 
+    fprintf(stderr, "usage: reverse <input> <output>\n"); 
+    exit(1); }
+
 }
